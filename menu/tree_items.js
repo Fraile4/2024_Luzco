3ff3531d30d6dc608c7a45e5da781c8c9130a4b2
ig000000 <igallegos001@ikasle.ehu.eus>
2024-03-16 23:44:05 +0100

    4. entregarako

--- menu/tree_items.js
+++ menu/tree_items.js
@@ -118,7 +118,7 @@ var TREE_ITEMS = [
 					[' Proba Script-a', null],
 					[' Proben emaitzak', null],
 					[' User acceptace test (UAT)','../Proiektua/MemoriarenEranskinak/User-Accetptance-Test-Plan-Free-Word-Template.pdf'],
-					[' RFP erantzunen hasierako ebaluazioa','../Proiektua/MemoriarenEranskinak/Software projects proposals evaluation template.pdf'],
+					[' RFP erantzunen hasierako ebaluazioa','../Proiektua/MemoriarenEranskinak/Proposamena RFPekin parekatuta ebaluatzeko txantiloia.pdf'],
 
 				],
 				['Ikuskaritza', null,
